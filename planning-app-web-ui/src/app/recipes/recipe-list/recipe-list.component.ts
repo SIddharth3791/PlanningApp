3ef7ac4aaddbeaf18dbbe1292235394348200aff
Patel <sidpatel@deloitte.com>
2019-03-19 14:55:55 -0400

    Created working stub and respected models

--- planning-app-web-ui/src/app/recipes/recipe-list/recipe-list.component.ts
+++ planning-app-web-ui/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes:Recipe[] = [
+    new Recipe("Taco", "Best Taco", "https://www.tacobueno.com/assets/food/tacos/Taco_Crispy_Beef_990x725.jpg"),
+    new Recipe("Pizza", "Best Pizza", "https://www.cookingclassy.com/wp-content/uploads/2013/09/spinach-artichoke-pizza3-alt-color+srgb.jpg")
+  ];
+  
   constructor() { }
 
   ngOnInit() {
